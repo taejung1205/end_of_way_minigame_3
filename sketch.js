bc3b7077bde2d29c9ab8f3e4d4420c8b59be27cd
Taejung Kim <immcoc1@gmail.com>
2022-06-22 13:02:25 +0900

    fix: typo

--- sketch.js
+++ sketch.js
@@ -8,7 +8,7 @@ let redlineStart=0;
 let cnv;
 
 function setup() {
-  cnv = createCanvas(window.innterWidth, 640);
+  cnv = createCanvas(window.innerWidth, 640);
   cnv.elt.addEventListener('click', myTouchStarted)
 }
 
